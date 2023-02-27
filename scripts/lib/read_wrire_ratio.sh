204ac89a3180d7ca6a73337ce2a037664a0ec92a
Raja Soundaramourty <rajasoun@icloud.com>
2023-02-27 09:12:34 -0500

    read write ratio calculation using triggers

--- scripts/lib/read_wrire_ratio.sh
+++ scripts/lib/read_wrire_ratio.sh
@@ -3,7 +3,7 @@
 # Connect to MySQL database via Docker and execute script
 function prepare_for_read_write_ratio() {
     exit_if_mysql_writer_not_running
-    local rw_ratio_sql_script="${GIT_BASE_PATH}/sql/read_write_ratio.sql.sql"
+    local rw_ratio_sql_script="${GIT_BASE_PATH}/sql/read_write_ratio.sql"
     # Setup the database for read write ratio calculation using triggers
-    docker exec -i "mysql_writer" mysql --defaults-extra-file=/etc/mysql.client.cnf  < "$rw_ratio_sql_script" 
+    cat $rw_ratio_sql_script | envsubst | docker exec -i mysql_writer sh -c 'export MYSQL_PWD=root_password; mysql -u root'
 }
