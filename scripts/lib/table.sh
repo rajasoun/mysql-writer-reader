7abcd14288e6e3bafa82671a61605be573c3a770
Raja Soundaramourty <rajasoun@icloud.com>
2023-03-03 12:22:39 -0500

    test sql files

--- scripts/lib/table.sh
+++ scripts/lib/table.sh
@@ -1,8 +1,8 @@
 #/usr/bin/env bash 
 
 function print_table() {
-  # sql select query result
-  result="$1"
+    # sql select query result
+    result="$1"
     # column names
-    echo "$result" | column -t
+    echo "$result" | column -t | sed 's/^/\t/'
 }
